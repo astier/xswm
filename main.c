a49d6546e717763eee420466cd22143d31731b37
Aleksandrs Stier <aleks.stier@icloud.com>
2024-09-18 16:36:59 +0200

    Replace linked-list with array

--- main.c
+++ main.c
@@ -32,14 +32,7 @@ enum {
     WM_N
 };
 
-typedef struct Client {
-    const Window w;
-    struct Client *next;
-} Client;
-
 // Functions
-static Client * get_client(Window);
-static Client * get_parent(const Client *);
 static int xerror(Display *, XErrorEvent *);
 static void add(Window);
 static void delete(Window);
@@ -50,6 +43,12 @@ static void send_event(Window, Atom);
 static void set_state(Window, long);
 static void update_client_list(Window, Bool);
 static void update_client_list_stacking(void);
+
+// Clients-Functions
+static Bool is_managed(Window);
+static Bool is_top(Window);
+static unsigned long get_idx(Window);
+static void remove(Window);
 
 // Event-Handlers
 static void client_message(const XClientMessageEvent *);
@@ -67,30 +66,28 @@ static void quit(void);
 // Variables
 static Atom wm_atoms[WM_N], net_atoms[Net_N], XA_WM_CMD;
 static Bool running = True;
-static Client *head; // Top-window and start of a linked-list
 static Display *d;
-static int clients_n = 0;
 static int sh, sw; // screen-width and -height
 static Window r; // root-window
 
-Client * get_client(const Window w) {
-    Client *c;
-    for (c = head; c && c->w != w; c = c->next);
-    return c;
-}
+// Clients
+static Window *clients;
+static unsigned long clients_n = 0; // Number of elements in array
 
-Client * get_parent(const Client *c) {
-    if (!c || c == head)
-        return NULL;
-    Client *p;
-    for (p = head; p && p->next != c; p = p->next);
-    return p;
+Bool is_managed(const Window w) { return get_idx(w) < clients_n; }
+
+Bool is_top(const Window w) { return w == clients[clients_n - 1]; }
+
+unsigned long get_idx(const Window w) {
+    unsigned long i;
+    for (i = 0; i < clients_n && clients[i] != w; i++);
+    return i;
 }
 
 int xerror(Display *dpy, XErrorEvent *e) { (void) dpy; (void) e; return 0; }
 
 void add(const Window w) {
-    if (get_client(w))
+    if (is_managed(w))
         return;
     // Configure, map and focus window
     XChangeProperty(d, w, net_atoms[WMDesktop], XA_CARDINAL, 32,
@@ -104,8 +101,8 @@ void add(const Window w) {
     XMapWindow(d, w);
     focus(w);
     // Add window to list
-    memcpy(head = malloc(sizeof(Client)), &(Client) { w, head }, sizeof(Client));
-    clients_n++;
+    clients = realloc(clients, ++clients_n * sizeof(Window));
+    clients[clients_n - 1] = w;
     update_client_list(w, True);
     update_client_list_stacking();
 }
@@ -119,16 +116,20 @@ void focus(const Window w) {
     send_event(w, wm_atoms[TakeFocus]);
 }
 
+void remove(const Window w) {
+    const unsigned long idx = get_idx(w);
+    if (idx < clients_n)
+        memmove(&clients[idx], &clients[idx + 1], (--clients_n - idx) * sizeof(Window));
+}
+
 void pop(const Window w) {
-    Client *c = get_client(w);
-    if (!c || head == c)
+    if (is_top(w))
         return;
     focus(w);
     XRaiseWindow(d, w);
     // Update list
-    get_parent(c)->next = c->next;
-    c->next = head;
-    head = c;
+    remove(w);
+    clients[clients_n++] = w;
     update_client_list_stacking();
 }
 
@@ -169,37 +170,34 @@ void update_client_list(const Window w, const Bool add) {
         return;
     }
     // Get _NET_CLIENT_LIST
-    unsigned long n;
-    unsigned char *clients;
+    unsigned long nitems;
+    unsigned char *prop = NULL;
     XGetWindowProperty(d, r, net_atoms[ClientList],
         0, sizeof(Window), False, XA_WINDOW, &(Atom) {None},
-        &(int) {None}, &n, &(unsigned long) {None}, &clients);
-    if (!clients)
+        &(int) {None}, &nitems, &(unsigned long) {None}, &prop);
+    if (!prop)
         return;
+    Window *client_list = (Window *) prop;
+    // Get window-index
+    unsigned long i;
+    for (i = 0; i < nitems && client_list[i] != w; i++);
     // Remove w from _NET_CLIENT_LIST
-    int i;
-    for (i = 0; i < clients_n && clients[i] != w; i++);
-    for ( ; i < (int) n - 1; i++)
-        clients[i] = clients[i + 1];
-    n--;
-    // Update _NET_CLIENT_LIST
-    XChangeProperty(d, r, net_atoms[ClientList], XA_WINDOW, 32,
-        PropModeReplace, clients, (int) n);
-    XFree(clients);
+    if (i < nitems) {
+        memmove(&client_list[i], &client_list[i + 1], (--nitems - i) * sizeof(Window));
+        XChangeProperty(d, r, net_atoms[ClientList], XA_WINDOW, 32,
+            PropModeReplace, (unsigned char *) client_list, (int) nitems);
+    }
+    XFree(prop);
 }
 
 void update_client_list_stacking(void) {
-    Window clients[clients_n];
-    int i = clients_n - 1;
-    for (Client *c = head; c; c = c->next)
-        clients[i--] = c->w;
     XChangeProperty(d, r, net_atoms[ClientListStacking], XA_WINDOW, 32,
-        PropModeReplace, (unsigned char *) clients, clients_n);
+        PropModeReplace, (unsigned char *) clients, (int) clients_n);
 }
 
 void client_message(const XClientMessageEvent *e) {
     const Window w = e->window;
-    if (!get_client(w))
+    if (!is_managed(w))
         return;
     const Atom msg = e->message_type;
     if (msg == net_atoms[ActiveWindow])
@@ -215,9 +213,9 @@ void configure_notify(const XConfigureEvent *e) {
     if (w == r && (wh != sh || ww != sw)) {
         sh = wh;
         sw = ww;
-        for (Client *c = head; c; c = c->next)
-            resize(w);
-    } else if (get_client(w)) {
+        for (unsigned long i = 0; i < clients_n; i++)
+            resize(clients[i]);
+    } else if (is_managed(w)) {
         if (e->border_width != 0)
             XSetWindowBorderWidth(d, w, 0);
         if (e->x != 0 || e->y != 0 || wh != sh || ww != sw)
@@ -240,8 +238,9 @@ void configure_request(const XConfigureRequestEvent *e) {
 
 // Prevent bad clients from stealing focus
 void focus_in(const XFocusInEvent *e) {
-    if (head && head->w != e->window)
-        focus(head->w);
+    const Window w = clients[clients_n - 1];
+    if (w != e->window)
+        focus(w);
 }
 
 void property_notify(const XPropertyEvent *e) {
@@ -259,31 +258,23 @@ void property_notify(const XPropertyEvent *e) {
 
 void unmap_notify(const XUnmapEvent *e) {
     const Window w = e->window;
-    Client *c = get_client(w);
-    if (!c)
+    if (!is_managed(w))
         return;
     if (clients_n == 1)
         XChangeProperty(d, r, net_atoms[ActiveWindow],
             XA_WINDOW, 32, PropModeReplace, None, 0);
-    if (c != head)
-        get_parent(c)->next = c->next;
-    else if (!head->next)
-        head = NULL;
-    else {
-        head = head->next;
-        focus(head->w);
-    }
-    free(c);
-    clients_n--;
+    else if (is_top(w))
+        last();
+    remove(w);
     update_client_list(w, False);
     update_client_list_stacking();
     set_state(w, WithdrawnState);
     XDeleteProperty(d, w, net_atoms[WMDesktop]);
 }
 
-void close(void) { if (clients_n > 0) delete(head->w); }
+void close(void) { if (clients_n > 0) delete(clients[clients_n - 1]); }
 
-void last(void) { if (clients_n > 1) pop(head->next->w); }
+void last(void) { if (clients_n > 1) pop(clients[clients_n - 2]); }
 
 void quit(void) { running = False; }
 
@@ -387,6 +378,7 @@ int main(const int argc, const char *argv[]) {
             case UnmapNotify: unmap_notify(&e.xunmap); break;
         }
     }
-    // Clean-Up (clients are freed by OS)
+    // Clean-Up
+    free(clients);
     XCloseDisplay(d);
 }
