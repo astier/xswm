8d028541d69a11ea1935086f784efe1fa91d1684
Aleksandrs Stier <aleks.stier@icloud.com>
2024-09-20 00:43:39 +0200

    Reorder functions and variables

--- main.c
+++ main.c
@@ -45,21 +45,6 @@ typedef struct Client {
     struct Client *next;
 } Client;
 
-// Functions
-static Bool send_event(Window, Atom);
-static Client * get_client(Window);
-static Client * get_parent(const Client *);
-static int get_state(Window);
-static int xerror(Display *, XErrorEvent *);
-static void delete(Window);
-static void focus(Window);
-static void pop(Window);
-static void resize(Client *);
-static void set_frame_extents(Window);
-static void set_state(Window, long);
-static void update_client_list(Window, Bool);
-static void update_client_list_stacking(void);
-
 // Event-Handlers
 static void button_press(const XButtonPressedEvent *);
 static void client_message(const XClientMessageEvent *);
@@ -70,162 +55,39 @@ static void map_request(Window);
 static void property_notify(const XPropertyEvent *);
 static void unmap_notify(const XUnmapEvent *);
 
+// List-Functions
+static Client * get_client(Window);
+static Client * get_parent(const Client *);
+
 // Remote-Commands
 static void close(void);
 static void last(void);
 static void quit(void);
 
+// Window-Management
+static void delete(Window);
+static void focus(Window);
+static void pop(Window);
+static void resize(Client *);
+
+// X-Helpers
+static Bool send_event(Window, Atom);
+static int get_state(Window);
+static int xerror(Display *, XErrorEvent *);
+static void set_frame_extents(Window);
+static void set_state(Window, long);
+static void update_client_list(Window, Bool);
+static void update_client_list_stacking(void);
+
 // Variables
 static Atom wm_atoms[WM_N], net_atoms[Net_N], XA_WM_CMD;
 static Bool running = True;
-static Display *d;
-static unsigned int sw, sh; // screen-width and -height
-static unsigned int bw = 1; // border-width
-static Window r; // root-window
-
-// Linked-List
 static Client *head; // Top-window and start of a linked-list
+static Display *d;
 static int clients_n = 0; // Number of clients in list
-
-Bool send_event(const Window w, const Atom protocol) {
-    Atom *protocols;
-    int count;
-    if (!XGetWMProtocols(d, w, &protocols, &count))
-        return False;
-    Bool protocol_exists = False;
-    while (!protocol_exists && count--)
-        protocol_exists = protocols[count] == protocol;
-    XFree(protocols);
-    if (!protocol_exists)
-        return False;
-    XEvent e;
-    e.type = ClientMessage;
-    e.xclient.window = w;
-    e.xclient.message_type = wm_atoms[Protocols];
-    e.xclient.format = 32;
-    e.xclient.data.l[0] = (long) protocol;
-    e.xclient.data.l[1] = CurrentTime;
-    return XSendEvent(d, w, False, NoEventMask, &e);
-}
-
-Client * get_client(const Window w) {
-    Client *c;
-    for (c = head; c && c->w != w; c = c->next);
-    return c;
-}
-
-Client * get_parent(const Client *c) {
-    if (!c || c == head)
-        return NULL;
-    Client *p;
-    for (p = head; p && p->next != c; p = p->next);
-    return p;
-}
-
-int get_state(const Window w) {
-    int state = -1;
-    unsigned char *prop;
-    unsigned long nitems;
-    if (XGetWindowProperty(d, w, wm_atoms[State], 0L, 2L, False, wm_atoms[State],
-    &(Atom) {None}, &(int) {None}, &nitems, &(unsigned long) {None},
-    (unsigned char **) &prop) == Success) {
-        if (nitems > 0)
-            state = *(int *) prop;
-        XFree(prop);
-    }
-    return state;
-}
-
-int xerror(Display *dpy, XErrorEvent *e) { (void) dpy; (void) e; return 0; }
-
-void delete(const Window w) {
-    if (!send_event(w, wm_atoms[DeleteWindow]))
-        XKillClient(d, w);
-}
-
-void focus(const Window w) {
-    XSetInputFocus(d, w, RevertToPointerRoot, CurrentTime);
-    XChangeProperty(d, r, net_atoms[ActiveWindow], XA_WINDOW,
-        32, PropModeReplace, (unsigned char *) &w, 1);
-    send_event(w, wm_atoms[TakeFocus]);
-}
-
-void pop(const Window w) {
-    Client *c = get_client(w);
-    if (!c || head == c)
-        return;
-    focus(w);
-    XRaiseWindow(d, w);
-    // Update list
-    get_parent(c)->next = c->next;
-    c->next = head;
-    head = c;
-    update_client_list_stacking();
-}
-
-void resize(Client *c) {
-    int x = (int) -bw, y = (int) -bw;
-    unsigned int width = sw, height = sh;
-    if (c->floating) {
-        const unsigned int true_width = c->width  + bw * 2;
-        if (true_width < sw) {
-            x = (int) (sw - true_width) / 2;
-            width = c->width;
-        }
-        const unsigned int true_height = c->height  + bw * 2;
-        if (true_height < sh) {
-            y = (int) (sh - true_height) / 2;
-            height = c->height;
-        }
-    }
-    c->x = x, c->y = y, c->width = width, c->height = height;
-    XMoveResizeWindow(d, c->w, x, y, width, height);
-}
-
-void set_frame_extents(const Window w) {
-    XChangeProperty(d, w, net_atoms[FrameExtents], XA_CARDINAL, 32,
-        PropModeReplace, (unsigned char *) (long []) {bw, bw, bw, bw}, 4);
-}
-
-void set_state(const Window w, const long state) {
-    XChangeProperty(d, w, wm_atoms[State], wm_atoms[State], 32,
-        PropModeReplace, (unsigned char *) (long []) {state, None}, 2);
-}
-
-void update_client_list(const Window w, const Bool add) {
-    if (add) {
-        XChangeProperty(d, r, net_atoms[ClientList], XA_WINDOW, 32,
-            PropModeAppend, (unsigned char *) &w, 1);
-        return;
-    }
-    // Get _NET_CLIENT_LIST
-    unsigned long nitems;
-    unsigned char *prop = NULL;
-    XGetWindowProperty(d, r, net_atoms[ClientList], 0, sizeof(Window), False, XA_WINDOW,
-        &(Atom) {None}, &(int) {None}, &nitems, &(unsigned long) {None}, &prop);
-    if (!prop)
-        return;
-    Window *client_list = (Window *) prop;
-    // Get window-index
-    unsigned long i;
-    for (i = 0; i < nitems && client_list[i] != w; i++);
-    // Remove w from _NET_CLIENT_LIST
-    if (i < nitems) {
-        memmove(&client_list[i], &client_list[i + 1], (--nitems - i) * sizeof(Window));
-        XChangeProperty(d, r, net_atoms[ClientList], XA_WINDOW, 32,
-            PropModeReplace, (unsigned char *) client_list, (int) nitems);
-    }
-    XFree(prop);
-}
-
-void update_client_list_stacking(void) {
-    Window clients[clients_n];
-    int i = clients_n - 1;
-    for (Client *c = head; c; c = c->next)
-        clients[i--] = c->w;
-    XChangeProperty(d, r, net_atoms[ClientListStacking], XA_WINDOW, 32,
-        PropModeReplace, (unsigned char *) clients, clients_n);
-}
+static unsigned int bw = 1; // border-width
+static unsigned int sw, sh; // screen-width and -height
+static Window r; // root-window
 
 void button_press(const XButtonPressedEvent *e) {
     pop(e->window);
@@ -368,11 +230,151 @@ void unmap_notify(const XUnmapEvent *e) {
     XDeleteProperty(d, w, net_atoms[WMDesktop]);
 }
 
+Client * get_client(const Window w) {
+    Client *c;
+    for (c = head; c && c->w != w; c = c->next);
+    return c;
+}
+
+Client * get_parent(const Client *c) {
+    if (!c || c == head)
+        return NULL;
+    Client *p;
+    for (p = head; p && p->next != c; p = p->next);
+    return p;
+}
+
 void close(void) { if (clients_n > 0) delete(head->w); }
 
 void last(void) { if (clients_n > 1) pop(head->next->w); }
 
 void quit(void) { running = False; }
+
+void delete(const Window w) {
+    if (!send_event(w, wm_atoms[DeleteWindow]))
+        XKillClient(d, w);
+}
+
+void focus(const Window w) {
+    XSetInputFocus(d, w, RevertToPointerRoot, CurrentTime);
+    XChangeProperty(d, r, net_atoms[ActiveWindow], XA_WINDOW,
+        32, PropModeReplace, (unsigned char *) &w, 1);
+    send_event(w, wm_atoms[TakeFocus]);
+}
+
+void pop(const Window w) {
+    Client *c = get_client(w);
+    if (!c || head == c)
+        return;
+    focus(w);
+    XRaiseWindow(d, w);
+    // Update list
+    get_parent(c)->next = c->next;
+    c->next = head;
+    head = c;
+    update_client_list_stacking();
+}
+
+void resize(Client *c) {
+    int x = (int) -bw, y = (int) -bw;
+    unsigned int width = sw, height = sh;
+    if (c->floating) {
+        const unsigned int true_width = c->width  + bw * 2;
+        if (true_width < sw) {
+            x = (int) (sw - true_width) / 2;
+            width = c->width;
+        }
+        const unsigned int true_height = c->height  + bw * 2;
+        if (true_height < sh) {
+            y = (int) (sh - true_height) / 2;
+            height = c->height;
+        }
+    }
+    c->x = x, c->y = y, c->width = width, c->height = height;
+    XMoveResizeWindow(d, c->w, x, y, width, height);
+}
+
+Bool send_event(const Window w, const Atom protocol) {
+    Atom *protocols;
+    int count;
+    if (!XGetWMProtocols(d, w, &protocols, &count))
+        return False;
+    Bool protocol_exists = False;
+    while (!protocol_exists && count--)
+        protocol_exists = protocols[count] == protocol;
+    XFree(protocols);
+    if (!protocol_exists)
+        return False;
+    XEvent e;
+    e.type = ClientMessage;
+    e.xclient.window = w;
+    e.xclient.message_type = wm_atoms[Protocols];
+    e.xclient.format = 32;
+    e.xclient.data.l[0] = (long) protocol;
+    e.xclient.data.l[1] = CurrentTime;
+    return XSendEvent(d, w, False, NoEventMask, &e);
+}
+
+int get_state(const Window w) {
+    int state = -1;
+    unsigned char *prop;
+    unsigned long nitems;
+    if (XGetWindowProperty(d, w, wm_atoms[State], 0L, 2L, False, wm_atoms[State],
+    &(Atom) {None}, &(int) {None}, &nitems, &(unsigned long) {None},
+    (unsigned char **) &prop) == Success) {
+        if (nitems > 0)
+            state = *(int *) prop;
+        XFree(prop);
+    }
+    return state;
+}
+
+int xerror(Display *dpy, XErrorEvent *e) { (void) dpy; (void) e; return 0; }
+
+void set_frame_extents(const Window w) {
+    XChangeProperty(d, w, net_atoms[FrameExtents], XA_CARDINAL, 32,
+        PropModeReplace, (unsigned char *) (long []) {bw, bw, bw, bw}, 4);
+}
+
+void set_state(const Window w, const long state) {
+    XChangeProperty(d, w, wm_atoms[State], wm_atoms[State], 32,
+        PropModeReplace, (unsigned char *) (long []) {state, None}, 2);
+}
+
+void update_client_list(const Window w, const Bool add) {
+    if (add) {
+        XChangeProperty(d, r, net_atoms[ClientList], XA_WINDOW, 32,
+            PropModeAppend, (unsigned char *) &w, 1);
+        return;
+    }
+    // Get _NET_CLIENT_LIST
+    unsigned long nitems;
+    unsigned char *prop = NULL;
+    XGetWindowProperty(d, r, net_atoms[ClientList], 0, sizeof(Window), False, XA_WINDOW,
+        &(Atom) {None}, &(int) {None}, &nitems, &(unsigned long) {None}, &prop);
+    if (!prop)
+        return;
+    Window *client_list = (Window *) prop;
+    // Get window-index
+    unsigned long i;
+    for (i = 0; i < nitems && client_list[i] != w; i++);
+    // Remove w from _NET_CLIENT_LIST
+    if (i < nitems) {
+        memmove(&client_list[i], &client_list[i + 1], (--nitems - i) * sizeof(Window));
+        XChangeProperty(d, r, net_atoms[ClientList], XA_WINDOW, 32,
+            PropModeReplace, (unsigned char *) client_list, (int) nitems);
+    }
+    XFree(prop);
+}
+
+void update_client_list_stacking(void) {
+    Window clients[clients_n];
+    int i = clients_n - 1;
+    for (Client *c = head; c; c = c->next)
+        clients[i--] = c->w;
+    XChangeProperty(d, r, net_atoms[ClientListStacking], XA_WINDOW, 32,
+        PropModeReplace, (unsigned char *) clients, clients_n);
+}
 
 int main(const int argc, const char *argv[]) {
     if (!(d = XOpenDisplay(NULL)))
